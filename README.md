ff2242b4d9e2ba372a7a48f01a024c0638e2363e
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-07-29 18:06:42 -0400

    Added ability to create db user,pass and assign to db
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- README.md
+++ README.md
@@ -23,6 +23,10 @@ docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=P@55w0rd -p 3306:3306 mrlesmit
 ```
 docker run -d --name mysql -e MYSQL_DB=pdns -p 3306:3306 mrlesmithjr/mysql
 ```
+`define db, user, and password`
+```
+docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=P@55w0rd -e MYSQL_DB=phpipam -e MYSQL_USER=phpipam -e MYSQL_PASS=phpipam -p 3306:3306 mrlesmithjr/mysql
+```
 `define volume for persistent storage`
 ```
 docker run -d --name mysql -v ./data:/var/lib/mysql -p 3306:3306 mrlesmithjr/mysql
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-if [ -n "$MYSQL_ROOT_PASSWORD" -a ! -n "$MYSQL_DB" ]; then
+if [ -n "$MYSQL_ROOT_PASSWORD" -a ! -n "$MYSQL_DB" -a ! -n "$MYSQL_USER" -a ! -n "$MYSQL_PASS" ]; then
   ansible-playbook -i "localhost," -c local /docker-entrypoint.yml \
   --extra-vars "mysql_root_password=${MYSQL_ROOT_PASSWORD}"
 fi
-if [ -n "$MYSQL_ROOT_PASSWORD" -a -n "$MYSQL_DB" ]; then
+if [ -n "$MYSQL_ROOT_PASSWORD" -a -n "$MYSQL_DB" -a ! -n "$MYSQL_USER" -a ! -n "$MYSQL_PASS" ]; then
   ansible-playbook -i "localhost," -c local /docker-entrypoint.yml \
-  --extra-vars "mysql_root_password=${MYSQL_ROOT_PASSWORD},mysql_db=${MYSQL_DB}"
+  --extra-vars "mysql_root_password=${MYSQL_ROOT_PASSWORD} mysql_db=${MYSQL_DB}"
 fi
-if [ ! -n "$MYSQL_ROOT_PASSWORD" -a -n "$MYSQL_DB" ]; then
+if [ ! -n "$MYSQL_ROOT_PASSWORD" -a -n "$MYSQL_DB" -a ! -n "$MYSQL_USER" -a ! -n "$MYSQL_PASS" ]; then
   ansible-playbook -i "localhost," -c local /docker-entrypoint.yml \
   --extra-vars "mysql_db=${MYSQL_DB}"
 fi
+if [ -n "$MYSQL_ROOT_PASSWORD" -a -n "$MYSQL_DB" -a -n "$MYSQL_USER" -a -n "$MYSQL_PASS" ]; then
+  ansible-playbook -i "localhost," -c local /docker-entrypoint.yml \
+  --extra-vars "mysql_root_password=${MYSQL_ROOT_PASSWORD} mysql_db=${MYSQL_DB} mysql_user=${MYSQL_USER} mysql_pass=${MYSQL_PASS}"
+fi
+if [ ! -n "$MYSQL_ROOT_PASSWORD" -a -n "$MYSQL_DB" -a -n "$MYSQL_USER" -a -n "$MYSQL_PASS" ]; then
+  ansible-playbook -i "localhost," -c local /docker-entrypoint.yml \
+  --extra-vars "mysql_db=${MYSQL_DB} mysql_user=${MYSQL_USER} mysql_pass=${MYSQL_PASS}"
+fi
+
 sleep 5
 exec "$@"
